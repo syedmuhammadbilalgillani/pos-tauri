9fe249f74cdd693c2c3f239dfa0ce8604fccf69e
Syed Muhammad Bilal Gillani <145171612+syedmuhammadbilalgillani@users.noreply.github.com>
2026-04-22 21:35:02 +0500

    Remove debug output from dashboard page

--- app/t/dashboard/page.tsx
+++ app/t/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function DashboardPage() {
 
   return (
     <div className="p-4 max-w-full mx-auto overflow-hidden">
-      {JSON.stringify(session, null, 2)}
+      {/* {JSON.stringify(session, null, 2)} */}
       <button onClick={() => logout()}>Logout</button>
     </div>
   );
